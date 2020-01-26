3d9a3ca820360c879b6dfc1087f1a11b70366175
Ali Abbas <43527087+realabbas@users.noreply.github.com>
2020-01-26 15:25:00 +0530

    Implementatioon of new URL() and Hostname

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-'use strict';
-const got = require('got');
+"use strict";
+const got = require("got");
 
 const statusCodes = {
-	isUp: 1,
-	isDown: 2,
-	invalidDomain: 3
+  isUp: 1,
+  isDown: 2,
+  invalidDomain: 3
 };
 
 module.exports = async url => {
+  url = new URL(url);
+  url = encodeURIComponent(url.host);
 
-	url = new URL(url);
-	url = encodeURIComponent(url.host);
-	
-	const {body} = await got(`https://isitup.org/${url}.json`, {
-		json: true,
-		headers: {
-			'user-agent': 'https://github.com/sindresorhus/is-up'
-		}
-	});
+  const { body } = await got(`https://isitup.org/${url}.json`, {
+    json: true,
+    headers: {
+      "user-agent": "https://github.com/sindresorhus/is-up"
+    }
+  });
 
-	const statusCode = body.status_code;
+  const statusCode = body.status_code;
 
-	if (statusCode === statusCodes.invalidDomain) {
-		throw new Error('Invalid domain');
-	}
+  if (statusCode === statusCodes.invalidDomain) {
+    throw new Error("Invalid domain");
+  }
 
-	return statusCode === statusCodes.isUp;
+  return statusCode === statusCodes.isUp;
 };
